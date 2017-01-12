a37f0aae97d1ff83c06cfc9e820280e9a2358c81
hufern <vifridget@gmail.com>
2017-01-12 18:45:07 +0100

    asd

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
 var txt;
 var p;
+var b;
 function preload() {
-  //txt = loadStrings("test0.txt");
+	txt = loadStrings("test0.txt")
 }
 
 function setup() {
+<<<<<<< HEAD
+	noCanvas();
+	p = select("#p");
+	b = select("#button");
+	
+	cI = createInput()
+	b.mousePressed(writeinp)
+
+=======
   noCanvas();
   //p = select("#p");
   //p.html(txt);
   p = createP("updated branch");
   p = createP("updated bsranch");
   p.html("updated branch u");
+>>>>>>> origin/master
+}
+
+function writeinp() {
+	p.html(txt)
 }
