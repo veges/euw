0c9f5b27d93ea46ce75c523bbc50823c19124390
hufern <vifridget@gmail.com>
2017-01-12 12:46:41 +0100

    first upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var txt;
+var p;
+function preload() {
+  //txt = loadStrings("test0.txt");
+}
+
+function setup() {
+  noCanvas();
+  //p = select("#p");
+  //p.html(txt);
+  p = createP("asdf");
+  p.html("fda");
+}
